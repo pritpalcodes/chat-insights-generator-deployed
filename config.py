b4f99f5fd7879f5bcaad719584d471caa7ad5cc2
Pritpal Singh <pritpals.9826@gmail.com>
2024-09-02 06:47:39 +0530

    fixed the uploads page

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
--- wsgi.py
+++
@@ -1,5 +0,0 @@
-from waitress import serve
-from app import app  # Assuming your Flask app is in a file named 'app.py'
-
-if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8080)
